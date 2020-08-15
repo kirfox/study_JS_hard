2b5186d98bd48091cf67c9f7fa9d3611d9d59a58
Kirill <muxin.kirill2011@yandex.ru>
2020-08-15 10:59:51 +0300

    features: i did the seventh hard task

--- lesson07/script.js
+++ lesson07/script.js
@@ -3,20 +3,20 @@
 let out = document.getElementById('out');
 let day = new Date();
 let week = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
-
+//Find weekday 
 let today = day.getDay();
+
 week.splice(6,1, '<i>' +'суббота'+'</i>');
 week.splice(0,1, '<i>' +'воскресенье'+'</i>');
 
+week.splice(today,1, '<strong>' + week[today] + '</strong>');
+
+//output jn the screen weekdays
 week.forEach(function(item) {
- 
   out.innerHTML += item + '<br>';
-  
 });
-//  ('<strong>' + today + '</strong>'); 
 
 
-console.log(week );
 
-console.log(week[today]);
+
 
